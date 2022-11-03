836c9cb3c98558d6e8ad089d3fe4ca3755b0d3af
home-windows <mohammad.zabtkar@gmail.com>
2022-11-04 00:32:20 +0330

    add model -- add model to admin pannel

--- web/models.py
+++ web/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+from django.contrib.auth.models import User
 
+
+class Expense(models.Model):
+    text = models.CharField(max_length=250)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE,)
